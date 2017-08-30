0874c337220f48fc5b85326db1885f48ab557dc7
Jamie Taylor (on SSFJT2) <jamie.taylor@sheffield.ac.uk>
2017-08-30 17:03:21 +0100

    no message

--- dbconnector/__init__.py
+++ dbconnector/__init__.py
@@ -1,3 +1,8 @@
-from dbconnector.dbconnector import DBConnector
+try:
+    #py2
+    from dbconnector import DBConnector
+except:
+    #py3+
+    from dbconnector.dbconnector import DBConnector
 
 __all__ = ["DBConnector"]
